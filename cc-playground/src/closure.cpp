5e8a9f46cf6ab9f627a6bd270cbf5f44eb4ed7be
yukino <yukino161043261@gmail.com>
2024-12-25 09:13:29 +0800

    chore: Regular code maintenance

--- cc-playground/src/closure.cpp
+++ cc-playground/src/closure.cpp
@@ -18,46 +18,10 @@ auto outer() {
   return ret;
 }
 
-// int main() {
-//   auto counter = outer();
-//   // (*(counter.get()))();
-//   (*counter)();
-//   (*counter)();
-//   (*counter)();
-// }
-
-using ifunc = function<double(double, double)>;
-
-ifunc tco(const ifunc& f) {
-  double value;
-  bool active = false;
-  vector<pair<double, double>> accumulated;
-
-  return [f, &value, &active, &accumulated](double x, double y) -> double {
-    accumulated.emplace_back(x, y);
-    if (!active) {
-      active = true;
-      while (!accumulated.empty()) {
-        auto args = accumulated.back();
-        accumulated.pop_back();
-        value = f(args.first, args.second);
-      }
-      active = false;
-      return value;
-    }
-    return -1;
-  };
-}
-
-double foo2co(double x, double y) {
-  if (y > 0) {
-    return foo2co(x + 1, y - 1);  // tail call
-  } else {
-    return x;
-  }
-}
-
 int main() {
-  cout << foo2co(1, 1000000);
-  return 0;
+  auto counter = outer();
+  // (*(counter.get()))();
+  (*counter)();
+  (*counter)();
+  (*counter)();
 }
