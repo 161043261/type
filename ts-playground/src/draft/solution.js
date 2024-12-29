320ccd0962d150d5876b8fb88dafe82ae444d3e0
yukino <yukino161043261@gmail.com>
2024-12-30 00:27:05 +0800

    chore: Regular code maintenance

--- ts-playground/src/draft/solution.js
+++ ts-playground/src/draft/solution.js
@@ -1,17 +1,54 @@
-"use strict";
-function minimumCost(m, n, horizontalCut, verticalCut) {
-  horizontalCut.sort((a, b) => a - b);
-  verticalCut.sort((a, b) => a - b);
-  let ans = 0,
-    i = 0,
-    j = 0;
-  while (i < m - 1 || j < n - 1) {
-    if (j === n - 1 || (i < m - 1 && horizontalCut[i] < verticalCut[j])) {
-      ans += horizontalCut[i++] * (n - j); // 上下连边
-    } else {
-      ans += verticalCut[j++] * (m - i); // 左右连边
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isSubPath(head, root) {
+  // bfs
+  const queue = [];
+  queue.push(root);
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (bfs(head, node)) {
+      return true;
+    }
+    if (node.left) {
+      queue.push(node.left);
+    }
+    if (node.right) {
+      queue.push(node.right);
     }
   }
-  return ans;
+  return false;
 }
-console.log(minimumCost(6, 3, [2, 3, 2, 3, 1], [1, 2]));
+
+/**
+ * @param {ListNode} head
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function bfs(head, root) {
+  if (head === null) {
+    return true;
+  }
+  if (root === null || head.val !== root.val) {
+    return false;
+  }
+  // head !== null, root !== null
+  return bfs(head.next, root.left) || bfs(head.next, root.right);
+}
--- ts-playground/src/draft/solution.ts
+++ ts-playground/src/draft/solution.ts
@@ -1,22 +1,40 @@
-function minimumCost(
-  m: number,
-  n: number,
-  horizontalCut: number[],
-  verticalCut: number[],
-): number {
-  horizontalCut.sort((a, b) => a - b);
-  verticalCut.sort((a, b) => a - b);
-  let ans = 0,
-    i = 0,
-    j = 0;
-  while (i < m - 1 || j < n - 1) {
-    if (j === n - 1 || (i < m - 1 && horizontalCut[i] < verticalCut[j])) {
-      ans += horizontalCut[i++] * (n - j); // 上下连边
-    } else {
-      ans += verticalCut[j++] * (m - i); // 左右连边
+function rankTeams(votes: string[]): string {
+  let maxLen = 0;
+  votes.forEach((vote) => {
+    maxLen = Math.max(maxLen, vote.length);
+  });
+  const alpha2cnts = new Map<string, number[]>();
+  for (let i = 0; i < votes.length; i++) {
+    for (let j = 0; j < votes[i].length; j++) {
+      if (alpha2cnts.has(votes[i][j])) {
+        const cnts = alpha2cnts.get(votes[i][j])!;
+        cnts[j] = cnts[j] ? cnts[j] + 1 : 1;
+        alpha2cnts.set(votes[i][j], cnts);
+      } else {
+        const cnts = new Array<number>(maxLen).fill(0);
+        cnts[j] = 1;
+        alpha2cnts.set(votes[i][j], cnts);
+      }
     }
+  }
+  const alphaCnts = Array.from(alpha2cnts);
+  alphaCnts.sort((item1, item2) => {
+    const [alpha, cnts1] = item1;
+    const [beta, cnts2] = item2;
+    for (let i = 0; i < cnts1.length; i++) {
+      if (cnts1[i] === cnts2[i]) {
+        continue;
+      }
+      return -cnts1[i] + cnts2[i];
+    }
+    return alpha.charCodeAt(0) - beta.charCodeAt(0);
+  });
+  console.log(alphaCnts);
+  let ans = "";
+  for (let i = 0; i < alphaCnts.length; i++) {
+    ans += alphaCnts[i][0];
   }
   return ans;
 }
 
-console.log(minimumCost(6, 3, [2, 3, 2, 3, 1], [1, 2]));
+console.log(rankTeams(["BCA", "CAB", "CBA", "ABC", "ACB", "BAC"]));
