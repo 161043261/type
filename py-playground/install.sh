e04ac399a30932cef423391d77705a856e27e8b2
Tiancheng <yukino161043261@gmail.com>
2025-01-11 07:42:56 +0800

    chore: Regular code maintenance

--- py-playground/install.sh
+++ py-playground/install.sh
@@ -1,4 +1,4 @@
-# python -m venv ./venv
+python -m venv ./venv
 
 if [ "$OS" = "Windows_NT" ]; then
   vpython=./venv/Scripts/python
--- py-playground/pkg/inner.py
+++ py-playground/pkg/inner.py
@@ -1,13 +1,20 @@
+import sys
 import os
 
-print(os.getcwd())
-print(os.path.dirname(os.getcwd()))
+try:
+    if not sys.path.__contains__(os.getcwd()):
+        sys.path.append(os.getcwd())
+    # if not sys.path.__contains__(os.path.join(os.getcwd(), 'pkg')):
+    #     sys.path.append(os.path.join(os.getcwd(), 'pkg'))
+    if not sys.path.__contains__(os.path.dirname(os.getcwd())):
+        sys.path.append(os.path.dirname(os.getcwd()))
+        # sys.path.append('..') # 也可以
 
-import sys
+finally:
+    from solution import Solution
+    print(sys.path)
 
-# sys.path.append(os.path.dirname(os.getcwd()))
-sys.path.append('..')
-
-from solution import fn
-
-fn()
+sol = Solution()
+print(sol.generateKey(num1=1, num2=10, num3=1000))
+print(sol.generateKey(num1=987, num2=879, num3=798))
+print(sol.generateKey(num1=1, num2=2, num3=3))
