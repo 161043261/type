44656f78c728b1cd82f69790309e8fa4d27c4d32
Tiancheng <yukino161043261@gmail.com>
2025-01-28 12:14:40 +0800

    chore: Regular code maintenance

---
+++ awesome/vue-pro/src/components/KeepAliveParent.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import KeepAliveBoy from './KeepAliveBoy.vue'
+import KeepAliveGirl from './KeepAliveGirl.vue'
+
+const flag = ref(true)
+</script>
+
+<template>
+  <div>
+    <KeepAliveBoy v-if="flag"></KeepAliveBoy>
+    <KeepAliveGirl v-else></KeepAliveGirl>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
