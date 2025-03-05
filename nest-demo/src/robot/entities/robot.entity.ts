590f9cfe051ac174b5a69baa6aad8a8cec64458f
Tiancheng <yukino161043261@gmail.com>
2025-03-05 12:52:21 +0800

    chore: Regular code maintenance

--- nest-demo/src/robot/entities/robot.entity.ts
+++
@@ -1,9 +0,0 @@
-export class Robot {
-  id: number;
-  name: string;
-  address: string;
-  state: /* 1 | 2 | 3 | 4 | 5 */ number;
-  failureNum: number;
-  admin: string;
-  email: string;
-}
--- nest-demo/src/robot/robot.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RobotService } from './robot.service';
-import { RobotController } from './robot.controller';
-
-@Module({
-  controllers: [RobotController],
-  providers: [RobotService],
-})
-export class RobotModule {}
