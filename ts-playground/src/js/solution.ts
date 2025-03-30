64ee1c77e12d2378d50e47293d002b8597c8e73a
Tiancheng <yukino161043261@gmail.com>
2025-03-30 21:59:06 +0800

    chore: Regular code maintenance

--- ts-playground/src/js/solution.ts
+++ ts-playground/src/js/solution.ts
@@ -1,14 +1,35 @@
-function maximumOr(nums: number[], k: number): number {
-  const suf: number[] = new Array(nums.length).fill(0);
-  for (let i = nums.length - 1; i >= 0; i--) {
-    suf[i] = suf[i + 1] | nums[i + 1];
-  }
-  let ans = 0n /** BigInt */,
-    pre = 0n; /** BigInt */
-  for (let i = 0; i < nums.length; i++) {
-    const cur = pre | (BigInt(nums[i]) << BigInt(k)) | BigInt(suf[i]);
-    ans = cur > ans ? cur : ans;
-    pre |= BigInt(nums[i]);
-  }
-  return Number(ans);
+function generateParenthesis(n: number): string[] {
+  let leftCnt = 0,
+    rightCnt = 0;
+  let path = "";
+  const ans: string[] = [];
+  const backtrack = () => {
+    if (rightCnt > leftCnt) {
+      return;
+    }
+    if (path.length === 2 * n) {
+      leftCnt === rightCnt && ans.push(path);
+      return;
+    }
+
+    leftCnt++;
+    path += "(";
+    backtrack();
+    leftCnt--;
+    path = path.slice(0, path.length - 1);
+
+    rightCnt++;
+    path += ")";
+    backtrack();
+    rightCnt--;
+    path = path.slice(0, path.length - 1);
+
+  };
+
+  backtrack()
+  return ans;
 }
+
+const ans = generateParenthesis(3);
+
+console.log(ans);
